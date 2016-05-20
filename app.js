eee05ffc94e37c6fc530439a60dd8b22c841d470
hanks-zyh <zhangyuhan2014@gmail.com>
2016-05-20 13:55:00 +0800

    add rount views

--- app.js
+++ app.js
@@ -8,7 +8,31 @@ app.listen(port)
 
 console.log('server listening on poert:'+port)
 
-
+// index
 app.get('/',function (req,res) {
-    
+    res.render('index',{
+        title: '租房首页'
+    })
+})
+
+
+// list
+app.get('/list',function (req,res) {
+    res.render('index',{
+        title: '租房列表'
+    })
+})
+
+// detail
+app.get('/detail/{id}',function (req,res) {
+    res.render('index',{
+        title: '详情页'+id
+    })
+})
+
+// admin
+app.get('/admin',function (req,res) {
+    res.render('index',{
+        title: '管理员'
+    })
 })
