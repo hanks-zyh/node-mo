a3be8df5d27c09290866bb43aa639daa4244d552
hanks-zyh <zhangyuhan2014@gmail.com>
2016-05-20 14:31:03 +0800

    include for jade

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 var express = require('express')
+var path = require('path')
 var port = process.env.PORT || 3000
 var app = express()
 
-app.set('views','./views')
-app.set('view engine', 'jade')
+app.set('views','./views/pages')
+app.set('view engine', 'jade') 
+app.use(express.static(path.join(__dirname,'bower_components')))
 app.listen(port)
 
 console.log('server listening on poert:'+port)
@@ -11,7 +13,30 @@ console.log('server listening on poert:'+port)
 // index
 app.get('/',function (req,res) {
     res.render('index',{
-        title: '租房首页'
+        title: '租房首页',
+        movies: [
+            {
+                title: '变形金刚1',
+                _id: 1,
+                poster: 'https://img3.doubanio.com/view/movie_poster_cover/mpst/public/p2332503406.jpg'
+            },
+            {
+                title: '变形金刚2',
+                _id: 2,
+                poster: 'https://img3.doubanio.com/view/movie_poster_cover/mpst/public/p2332503406.jpg'
+            },
+            {
+                title: '变形金刚3',
+                _id: 3,
+                poster: 'https://img3.doubanio.com/view/movie_poster_cover/mpst/public/p2332503406.jpg'
+            },
+            {
+                title: '变形金刚4',
+                _id: 4,
+                poster: 'https://img3.doubanio.com/view/movie_poster_cover/mpst/public/p2332503406.jpg'
+            }
+            
+        ]
     })
 })
 
@@ -23,8 +48,8 @@ app.get('/list',function (req,res) {
     })
 })
 
-// detail
-app.get('/detail/{id}',function (req,res) {
+// movie detail
+app.get('/movie/:id',function (req,res) {
     res.render('index',{
         title: '详情页'+id
     })
