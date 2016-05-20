1c911f951fda5615027b6e4a8a9d979715003b58
hanks-zyh <zhangyuhan2014@gmail.com>
2016-05-20 13:38:24 +0800

    create dit

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require('express')
+var port = process.env.PORT || 3000
+var app = express()
+
+app.set('views','./views')
+app.set('view engine', 'jade')
+app.listen(port)
+
+console.log('server listening on poert:'+port)
+
+
+app.get('/',function (req,res) {
+    
+})
